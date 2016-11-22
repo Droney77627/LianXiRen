ea7d78528339b9444586ca4c58bb4f2d68e5c4de
Droney <D@maggietekimacbook-air.local>
2016-11-22 23:59:37 +0800

    Create and set nav

--- LianXiRen/ViewController.m
+++ LianXiRen/ViewController.m
@@ -17,8 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setNav];
 }
-
+- (void)setNav{
+    
+    self.title = @"通讯录";
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"添加" style:UIBarButtonItemStylePlain target:self action:@selector(rightBtnClick)];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
